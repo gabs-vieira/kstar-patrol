ac42769c7cacc8267a7bd0356817a669fe4e720b
rafael-graunke <rafael.graunke@outlook.com>
2024-11-15 17:53:02 -0300

    feat: sector and ally ships

--- main.asm
+++ main.asm
@@ -21,6 +21,25 @@
 
     string_length equ $-string
 
+    sector_one  db 5 dup(" "), " ___       _              _    ",13,10
+                db 5 dup(" "), "/ __| ___ | |_ ___ _ _   / |   ",13,10
+                db 5 dup(" "), "\__ \/ -_)|  _/ _ \ '_|  | |   ",13,10
+                db 5 dup(" "), "|___/\___\ \__\___/_|    |_|   ",13,10
+
+    sector_two  db 5 dup(" "), " ___       _              ___  ",13,10
+                db 5 dup(" "), "/ __| ___ | |_ ___ _ _   |_  ) ",13,10
+                db 5 dup(" "), "\__ \/ -_)|  _/ _ \ '_|   / /  ",13,10
+                db 5 dup(" "), "|___/\___\ \__\___/_|    /___| ",13,10
+
+    sector_three    db 5 dup(" "), " ___       _              ____ ",13,10
+                    db 5 dup(" "), "/ __| ___ | |_ ___ _ _   |__ / ",13,10
+                    db 5 dup(" "), "\__ \/ -_)|  _/ _ \ '_|   |_ \ ",13,10
+                    db 5 dup(" "), "|___/\___\ \__\___/_|    |___/ ",13,10
+
+    sector_len equ $-sector_three
+
+    sector_vec dw offset sector_one, offset sector_two, offset sector_three
+
     btn_iniciar db  14 dup(" "),218,196,196,196,196,196,196,196,196,196,191,13,10
                  db 14 dup(" "),179," INICIAR ",179,10,13
                  db 14 dup(" "),192,196,196,196,196,196,196,196,196,196,217,13,10
@@ -55,6 +74,11 @@
 
     ship_pos dw 0
     
+    allies_pos_vec dw 320 * 20, 320 * 40, 320 * 60, 320 * 80, 320 * 100, 320 * 120, 320 * 140, 320 * 160
+    
+    ; Least significant nibble represents the color, most significant nibble represents dead/alive (1/0)
+    allies_attr_vec db 15H, 16H, 19H, 1AH, 1BH, 1CH, 1DH, 1EH
+
     alien_ship  db 0,0,0,0,0,0,0,0,9,9,9,9,9,9,9
                 db 0,0,0,0,0,0,0,0,9,9,0,0,0,0,0
                 db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
@@ -89,7 +113,7 @@ HANDLE_INPUT PROC
     jmp END_HANDLE
 
 ARROW_UP:
-    mov ah, 0
+    xor ah, ah
     mov menu, ah
 
     jmp RENDER_BUTTONS
@@ -100,7 +124,7 @@ ARROW_DOWN:
 
 RENDER_BUTTONS:
     mov al, screen
-    cmp al, 0H
+    cmp al, 0
     jne END_HANDLE
     call PRINT_BUTTONS
 
@@ -109,41 +133,69 @@ END_HANDLE:
 ENDP
 
 PRINT_TITLE_MENU proc
-    mov AX, DS 
-    mov ES, AX
+    mov ax, ds 
+    mov es, ax
 
-    mov BP, OFFSET string
-    mov CX, string_length ; tamanho
-    mov BL, 02H ; Cor verde (se bit 1 de AL estiver limpo, usamos BL)
-    mov DX, 0 ;linha / coluna
+    mov bp, offset string
+    mov cx, string_length ; tamanho
+    mov bl, 02H ; Cor verde (se bit 1 de AL estiver limpo, usamos BL)
+    xor dx, dx ;linha / coluna
     call PRINT_STRING
 
     ret
 PRINT_TITLE_MENU endp
 
+; BH = video page
 PRINT_STRING PROC
     push AX
     push BX
-    push CX
-    push DX
+    push DS
+    push ES
     push SI
     push BP
 
     ; Configura os parâmetros para a função 13h
-    mov AH, 13h         ; Função para escrever string com atributos de cor
-    mov AL, 1           ; Modo: atualiza cursor após a escrita
+    mov ah, 13h         ; Função para escrever string com atributos de cor
+    mov al, 1           ; Modo: atualiza cursor após a escrita
                         ; AL = 1 -> modo de atualização de cursor
-    mov BH, 0           ; Página de vídeo 0
+    xor bh, bh           ; Página de vídeo 0
     int 10h             ; Chamada de interrupção para exibir a string
 
     pop BP
     pop SI
-    pop DX
-    pop CX
+    pop ES
+    pop DS
     pop BX
     pop AX
     ret
 ENDP
+
+; SI = sprite pointer
+; BL = color
+CHANGE_SPRITE_COLOR proc
+    push ax
+    push bx
+    push cx
+    push si
+
+    mov cx, 15*9
+
+PIXEL_LOOP:
+    mov al, [si]
+    cmp al, 0
+    jz SKIP_REPLACE
+    mov [si], bl
+
+SKIP_REPLACE:
+    inc si
+    loop PIXEL_LOOP
+
+    pop si
+    pop cx
+    pop bx
+    pop ax
+    ret
+endp
 
 ; AX = sprite position
 ; SI = sprite pointer
@@ -156,8 +208,6 @@ RENDER_SPRITE proc
     push ds
     push ax
 
-
-        
     mov ax, @data
     mov ds, ax
 
@@ -197,10 +247,10 @@ PRINT_BUTTONS proc
 
 START_BTN:
     ; Exibe o botão INICIAR
-    mov BP, OFFSET btn_iniciar
-    mov CX, btn_iniciar_length ; tamanho
-    mov DL, 0 ; coluna
-    mov DH, 18 ; linha
+    mov bp, offset btn_iniciar
+    mov cx, btn_iniciar_length ; tamanho
+    xor dl, dl ; coluna
+    mov dh, 18 ; linha
     call PRINT_STRING
 
     mov bl, 0FH
@@ -210,11 +260,10 @@ START_BTN:
     mov bl, 0CH
 
 EXIT_BTN:
-    ; Exibe o botão SAIR
-    mov BP, OFFSET btn_sair
-    mov CX, btn_sair_length ; tamanho
-    mov DL, 0 ; coluna
-    mov DH, 21 ; linha
+    mov bp, offset btn_sair
+    mov cx, btn_sair_length
+    xor dl, dl ; coluna
+    mov dh, 21 ; linha
     call PRINT_STRING
 
     pop ax
@@ -250,7 +299,7 @@ CROSS_SHIPS proc
     call RENDER_SPRITE
 
     xor cx, cx
-    mov dx, 4000H
+    mov dx, 0C350H
     mov ah, 86H
     int 15h
     jmp END_POS_UPDATE
@@ -272,7 +321,7 @@ MOVE_ALIEN_SHIP:
     call RENDER_SPRITE
     
     xor cx, cx
-    mov dx, 4000H
+    mov dx, 0C350H
     mov ah, 86H
     int 15h
     jmp END_POS_UPDATE
@@ -284,14 +333,130 @@ END_POS_UPDATE:
     ret
 endp
 
-; Procedimento principal
-main proc; Muda modo grafico
+RENDER_ALLY_SHIPS proc
+    push dx
+    push cx
+    push bx
+    push ax
+
+    mov cx, 8
+    mov bx, offset allies_pos_vec
+    mov dx, offset allies_attr_vec
+
+RENDER_SINGLE:
+    mov ax, [bx]
+    mov si, offset ship
+
+    push bx
+    push ax
+
+    mov bx, dx
+    mov al, [bx]
+    mov bl, al
+    and bl, 0FH
+    and al, 0F0H
+    jnz NO_CLEAR
+    mov si, offset empty_sprite
+
+NO_CLEAR:
+    pop ax
+    call CHANGE_SPRITE_COLOR
+    call RENDER_SPRITE
+
+    pop bx
+    add bx, 2
+    inc dx
+    loop RENDER_SINGLE
+
+    pop ax
+    pop bx
+    pop cx
+    pop dx
+    ret
+endp
+
+CLEAR_SCREEN proc
+    push ax
+    push cx
+    push es
+    push di
+
+    mov ax,0A000h
+    mov es,ax
+    xor di, di
+    mov cx, 32000d
+    cld
+    xor ax, ax
+    rep stosw
+    
+    pop di
+    pop es
+    pop cx
+    pop ax
+    ret
+CLEAR_SCREEN endp
+
+RENDER_SECTOR proc
+    push ax
+    push bx
+    push cx
+    push dx
+    push bp
+
+    call CLEAR_SCREEN
+
+    ; Print Sector
+    mov al, sector
+    xor ah, ah
+    dec al ; number vector index
+
+    shl al, 1 ; multiply by 2 (since num_vec values are dw)
+    mov bx, offset sector_vec ; get the vector
+    add bx, ax ; add the index to the vector ptr
+    mov bp, [bx] ; set BP to base address of number
+    mov cx, sector_len
+    xor dl, dl; line
+    mov dh, 10
+    mov bl, 0DH
+    call PRINT_STRING
+
+    pop bp
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+endp
+
+RESET proc ; Contains all procedures for reseting values
+
+    ret
+endp
+
+UPDATE proc ; Contains all procedures for updating game state
+    ; call UPDATE_SHIP
+    ret
+endp
+
+RENDER proc ; Contains all procedures for rendering game objects
+    ; call RENDER_SHIP
+    ; call RENDER_SCORE
+    call RENDER_ALLY_SHIPS
+
+    ret
+endp
+
+
+MAIN proc
     mov AX, @data
     mov DS, AX
+    mov AX, 0A000H
     mov ES, AX
+    xor DI, DI
 
     ; Define o modo de video
-    mov ah, 0h
+    xor ah, ah
+    xor bh, bh
     mov al, 13h
     int 10h
 
@@ -316,37 +481,46 @@ MENU_LOOP:
     je SELECT_OPTION
 
     ; Retorno ao loop do menu
-    mov ah, 0H
+    xor ah, ah
     int 16H
     jmp MENU_LOOP
 
 SELECT_OPTION:
-    mov ah, 0H
+    xor ah, ah
     int 16H
     
     mov ah, menu
     cmp ah, 1
-    je ENCERRA
+    je FINISH
+
+    call RENDER_SECTOR
+
+    ; Wait 4s
+    mov cx, 3DH
+    mov dx, 900H
+    mov ah, 86H
+    int 15h
+
+    call CLEAR_SCREEN
 
 GAME_LOOP:
-    ; call PRINT_SECTOR
-    mov ax, 6562
-    mov si, offset ship
+    call UPDATE
+    call RENDER
 
-    ; jmp MENU_LOOP
+    jmp GAME_LOOP
 
 
-ENCERRA:
+FINISH:
     ; Volta para modo texto
-    mov ah, 0h
+    xor ah, ah
     mov al, 3h
     int 10h
 
     ; Encerra o programa
     mov ah, 4ch
-    mov al, 0
+    xor al, al
     int 21h
     ret
-main endp
+endp
 
-end main
+end MAIN
